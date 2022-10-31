025f1b789ef96a7ab90836e95ece34e40146efae
Piotr Kasperek <piotrkasperek@MacBook-Pro-Piotr.local>
2022-10-31 20:18:17 +0100

    implement access token and refresh token with strategies

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,20 @@
 import { CreateUserDto } from './dto/auth.dto';
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { User } from '@prisma/client';
+import { Tokens } from './types/tokens.types';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signUp(@Body() dto: CreateUserDto): Promise<User> {
+  signUp(@Body() dto: CreateUserDto): Promise<Tokens> {
     return this.authService.signUp(dto);
   }
 
   @Post('signin')
   @HttpCode(HttpStatus.OK)
-  signIn(@Body() dto: CreateUserDto): Promise<User> {
+  signIn(@Body() dto: CreateUserDto): Promise<Tokens> {
     return this.authService.signIn(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,18 @@
+import { ConfigService } from '@nestjs/config';
 import { CreateUserDto } from './dto/index';
 import { ForbiddenException, Injectable } from '@nestjs/common';
 import * as argon from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signUp(dto: CreateUserDto) {
     const hash = await argon.hash(dto.password);
@@ -18,8 +24,9 @@ export class AuthService {
           hash,
         },
       });
-      delete user.hash;
-      return user;
+      const tokens = await this.getTokens(user.id, user.email);
+      await this.updateRthash(user.id, tokens.refresh_token);
+      return tokens;
     } catch (error) {
       if (error instanceof PrismaClientKnownRequestError) {
         if (error.code === 'P2002') {
@@ -36,9 +43,51 @@ export class AuthService {
         email: dto.email,
       },
     });
-    if (!user) throw new ForbiddenException('User not exists');
+    if (!user) throw new ForbiddenException('Access Denied!');
     const comparePass = await argon.verify(user.hash, dto.password);
     if (!comparePass) throw new ForbiddenException('Password is wrong');
-    return user;
+    const tokens = await this.getTokens(user.id, user.email);
+    await this.updateRthash(user.id, tokens.refresh_token);
+    return tokens;
+  }
+
+  async getTokens(userId: number, email: string) {
+    const accessToken = this.jwtService.signAsync(
+      {
+        sub: userId,
+        email,
+      },
+      {
+        secret: this.config.get('SECRET_KEY'),
+        expiresIn: 60 * 15,
+      },
+    );
+    const refreshToken = this.jwtService.signAsync(
+      {
+        sub: userId,
+        email,
+      },
+      {
+        secret: this.config.get('SECRET_KEY'),
+        expiresIn: 60 * 60 * 24 * 7,
+      },
+    );
+
+    return {
+      access_token: await accessToken,
+      refresh_token: await refreshToken,
+    };
+  }
+
+  async updateRthash(userId: number, refreshToken: string): Promise<void> {
+    const hash = await argon.hash(refreshToken);
+    await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        hashedRt: hash,
+      },
+    });
   }
 }
