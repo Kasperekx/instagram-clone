fe9fef4cddf78bec855b457f03ba386033712f87
Piotr Kasperek <piotrkasperek@MacBook-Pro-Piotr.local>
2022-10-31 17:23:27 +0100

    adding reeturn void type

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,20 @@
 import { CreateUserDto } from './dto/auth.dto';
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { User } from '@prisma/client';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signUp(@Body() dto: CreateUserDto) {
+  signUp(@Body() dto: CreateUserDto): Promise<User> {
     return this.authService.signUp(dto);
   }
 
   @Post('signin')
   @HttpCode(HttpStatus.OK)
-  signIn(@Body() dto: CreateUserDto) {
+  signIn(@Body() dto: CreateUserDto): Promise<User> {
     return this.authService.signIn(dto);
   }
 }
